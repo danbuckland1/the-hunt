b3f5e6f3200a50839c3e1e8e8ab03641b8185691
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-10-15 21:49:43 -0400

    Connection to firebase established

--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -1,17 +1,66 @@
 import React, { Component } from "react";
+import fire from "../firebase.js";
 import Wrapper from "../components/Wrapper";
 import Navbar from "../components/Navbar";
+import StreamChat from "../components/StreamChat";
+
+
 
 class Activity extends Component {
 
-render(){
-    return (
-        <Wrapper>
-            <Navbar />
-            <p>Chat/Activity Stream will go here.</p>
-        </Wrapper>
-    );
-};
+    state = {
+        user: "",
+        msg: ""
+    }
+
+    captureUser = (event) => 
+    {
+        let user = event.target.value;
+        this.setState({
+            user: user
+        });
+        console.log(user);
+    };
+
+    captureMsg = (event) => 
+    {
+        let msg = event.target.value;
+        this.setState({
+            msg: msg
+        });
+        console.log(msg);
+        
+    };
+
+
+    handleMsgSubmit = (event) => {
+        event.preventDefault();
+        const chatRef = fire.database().ref();
+        let chat = {
+            username: this.state.user,
+            text: this.state.msg
+        };
+        chatRef.push(chat);
+        this.setState({
+            user: "",
+            msg: ""
+        });
+        console.log("submitted!");
+    };
+
+
+    render(){
+        return (
+            <Wrapper>
+                <Navbar />
+                <StreamChat 
+                captureUser={this.captureUser}
+                captureMsg={this.captureMsg} 
+                handleMsgSubmit={this.handleMsgSubmit}
+                />
+            </Wrapper>
+        );
+    };
 
 
 
