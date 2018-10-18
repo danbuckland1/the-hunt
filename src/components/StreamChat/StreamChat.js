76667dbdc2593e6dc79a4a088d9feaa45b78cf08
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-10-17 21:58:19 -0400

    Retrieved data from firebase and logged chat history to console

--- src/components/StreamChat/StreamChat.js
+++ src/components/StreamChat/StreamChat.js
@@ -4,10 +4,7 @@ import "./StreamChat.css";
 const StreamChat = props => (
     <div className="stream-chat">
         <div id="streamWindow">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut sem ipsum. Aliquam vulputate fringilla nisi, ut mattis sem auctor et. In cursus vitae leo et gravida. Praesent id massa arcu. In in sapien et justo venenatis dignissim. In pulvinar tortor nisl, vel rhoncus orci mollis non. Nulla aliquam, leo sit amet volutpat luctus, lorem libero iaculis tortor, quis lacinia ex ipsum ac nulla. Nullam pharetra efficitur arcu, vel pretium erat facilisis a. Etiam consequat tortor urna, eu consequat nisl pretium a. </p>
-
-        <p>Donec consequat et arcu non maximus. Nunc eget augue nec turpis auctor aliquet at sed turpis. Vestibulum ut vestibulum orci. Fusce maximus aliquet mi vitae efficitur. Nunc cursus metus id velit ullamcorper consequat. Nunc ultricies dolor mi, vel elementum magna ullamcorper laoreet. Fusce lacinia felis sit amet augue convallis varius. Aenean sed sollicitudin tellus. Vivamus at tellus diam. Vestibulum viverra efficitur erat, eget molestie nunc. Duis mattis, libero sit amet euismod efficitur, sapien ante tristique libero, sagittis efficitur mi lectus sed metus. Suspendisse potenti. Donec congue et felis vel consequat. Quisque ut erat nec tortor suscipit condimentum et a metus.</p>
-
+            <p className="line" id={props.id}>{props.user + ": " + props.text}</p>
         </div>
         <hr />
         <form>
--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -4,46 +4,71 @@ import Wrapper from "../components/Wrapper";
 import Navbar from "../components/Navbar";
 import StreamChat from "../components/StreamChat";
 
+//references the firebase database
+const database = fire.database().ref("log");
 
 
 class Activity extends Component {
-
+    //Sets State
     state = {
-        user: "",
-        msg: ""
+        team: "",
+        txt: "",
+        chatHistory: {}
     }
 
-    captureUser = (event) => 
-    {
-        let user = event.target.value;
-        this.setState({
-            user: user
+    //Will load chat history to window
+    componentDidMount() {
+        database.on("value", snapshot => {
+            this.setState({
+                chatHistory: snapshot.val()
+            });
+            let log = snapshot.val();
+            let keys = Object.keys(log);
+            console.log(keys);
+            for (let i = 0; i < keys.length; i++) {
+                let k = keys[i];
+                let teamName = log[k].teamName;
+                let text = log[k].text;
+                console.log(teamName + ": " + text);
+            }
         });
-        console.log(user);
+    }
+
+    //Captures Username from input field
+    captureTeam = (event) => 
+    {
+        let team = event.target.value;
+        //adds username to the state
+        this.setState({
+            team: team
+        });
     };
 
+    //Captures Message from input field
     captureMsg = (event) => 
     {
-        let msg = event.target.value;
+        let txt = event.target.value;
+        //adds message to the state
         this.setState({
-            msg: msg
-        });
-        console.log(msg);
-        
+            txt: txt
+        }); 
     };
 
 
+    //Takes captured team/message and sends it to firebase
     handleMsgSubmit = (event) => {
         event.preventDefault();
-        const chatRef = fire.database().ref();
-        let chat = {
-            username: this.state.user,
-            text: this.state.msg
+        //Create chat object that will take the username/text from the state
+        let message = {
+            teamName: this.state.team,
+            text: this.state.txt
         };
-        chatRef.push(chat);
+        //pushes message object to firebase
+        database.push(message);
+        //clears the state for future messages
         this.setState({
-            user: "",
-            msg: ""
+            team: "",
+            txt: ""
         });
         console.log("submitted!");
     };
@@ -54,9 +79,12 @@ class Activity extends Component {
             <Wrapper>
                 <Navbar />
                 <StreamChat 
-                captureUser={this.captureUser}
+                captureUser={this.captureTeam}
                 captureMsg={this.captureMsg} 
                 handleMsgSubmit={this.handleMsgSubmit}
+                id = {this.keys}
+                user = {this.name}
+                text = {this.text}
                 />
             </Wrapper>
         );
