30c6ca47140c97d307bfd5b56afcf256288291c5
danbuckland1 <buckd409@gmail.com>
2018-10-19 22:35:46 -0400

    figuring out auth redirect

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -24,3 +24,20 @@ passport.use(
     }
   )
 );
+
+// Redirect the user to the OAuth provider for authentication.  When
+// complete, the provider will redirect the user back to the application at
+//     /auth/provider/callback
+app.get("/auth/google", passport.authenticate("google"));
+
+// The OAuth provider has redirected the user back to the application.
+// Finish the authentication process by attempting to obtain an access
+// token.  If authorization was granted, the user will be logged in.
+// Otherwise, authentication has failed.
+app.get(
+  "/auth/google/redirect",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
