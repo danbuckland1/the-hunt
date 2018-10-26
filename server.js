5726d36941e9e88d333a7e946281b63bb267f254
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-10-25 20:57:29 -0400

    Last commit before merging to mao master branch

---
+++ server.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const express = require("express");
+const firebase = require("firebase");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const react = require("react");
+const reactDom = require("react-dom");
+const reactRouterDom = require("react-router-dom");
+// const reactScripts = require("react-scripts");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+//Connecting to mongodb
+mongoose.connect(keys.mongodb.dbURI, () =>{
+  console.log('connected to mongodb');
+});
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
