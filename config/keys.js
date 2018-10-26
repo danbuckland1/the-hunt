21d867b666f0a06e120e9b46e9af7a36fae5a48a
danbuckland1 <buckd409@gmail.com>
2018-10-25 21:55:12 -0400

    Auth works after code rework

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,15 @@ module.exports = {
     google: {
         clientID: "324068151974-qcq34tlplhkusplocqrf84ap8muv9f4p.apps.googleusercontent.com",
         clientSecret: "kJd7ZJTC4g2t_uZ9ATZLz7UX"
-    }
+    
+},
+
+mongodb: {
+    dbURI: 'mongodb://DanRoot:danroot1@ds111063.mlab.com:11063/the-hunt'
+},
+
+session: {
+    cookieKey: 'this is the cookie key'
 }
+
+};
